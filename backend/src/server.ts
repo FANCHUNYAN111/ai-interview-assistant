5c0bed6b48e6cc3cf258d68d552d211bb00348e3
F_chunyan <fanchunyan@conta-ai.com>
2026-05-22 13:47:04 +0800

    1

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,63 @@
-
 import express from "express";
+
 import cors from "cors";
+
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
-import interviewRoutes from "./routes/interviewRoutes.js";
-import conversationRoutes from "./routes/conversationRoutes.js";
+import authRoutes from "./routes/authRoutes";
+
+import interviewRoutes from "./routes/interviewRoutes";
+
+import conversationRoutes from "./routes/conversationRoutes";
+
 dotenv.config();
 
 const app = express();
 
-// app.use(cors());
+/**
+ * 中间件
+ */
 
-app.use("/api/auth", authRoutes);
-app.use("/api/interview", interviewRoutes);
-app.use(
-  "/api/conversations",
-  conversationRoutes
-);
+// 先 cors
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
 
-      "https://ai-interview-assistant.up.railway.app/",
+      "https://ai-interview-assistant.vercel.app",
     ],
 
     credentials: true,
   })
 );
+
+// 再 json
 app.use(express.json());
 
+/**
+ * 路由
+ */
+
+app.use("/api/auth", authRoutes);
+
+app.use("/api/interview", interviewRoutes);
+
+app.use(
+  "/api/conversations",
+  conversationRoutes
+);
+
+/**
+ * 测试接口
+ */
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
